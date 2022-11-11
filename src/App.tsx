29af39f3a0c24d9849441cc3f3f0a7fe3b8a9d4a
Aeolus.Zhang-张敬峥 <Aeolus.Zhang@fanruan.com>
2022-11-11 15:55:35 +0800

    升级18 reset

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,32 @@ import { atom, useAtom } from "jotai";
 import logo from "./logo.svg";
 import "./App.css";
 import { DarkMode } from "./DarkMode";
+import Input from "./Todo";
+import { Redux } from "./Redux";
+import { atomWithReset, useResetAtom } from "jotai/utils";
 
-export const countAtom = atom(1); // 商品个数
-const priceAtom = atom(67);
+
+export const countAtom = atomWithReset(1); // 商品个数
+const priceAtom = atomWithReset(67);
 const totalAtom = atom((get) => get(countAtom) * get(priceAtom));
 
+const Total = () => {
+  const [total] = useAtom(totalAtom);
+  const [count, setCount] = useAtom(countAtom);
+  const reset = useResetAtom(countAtom);
+  return (
+    <>
+      <div>总价：{total}</div>
+      <button onClick={() => setCount((count) => count + 1)} >商品个数：{count}</button>
+      <button onClick={() => {
+        reset();
+      }}>重置(useResetAtom在React17上不支持)</button>
+    </>
+  );
+};
+
 const Price = () => {
-  const price = useAtom(priceAtom);
+  const [price] = useAtom(priceAtom);
   const count = useAtom(countAtom);
   return (
     <>
@@ -17,27 +36,18 @@ const Price = () => {
   );
 };
 
-const Total = () => {
-  const total = useAtom(totalAtom);
-  const count = useAtom(countAtom);
-  return (
-    <>
-      <div>总价：{total}</div>
-      <button>商品个数：{count}</button>
-    </>
-  );
-};
-
-
 function App() {
-  const [count, setCount] = useAtom(countAtom);
+  const [count] = useAtom(countAtom);
   return (
     <div className={'App transition duration-700 bg-white dark:bg-[#282c34] dark:text-white'}>
       <header className="App-header flex flex-col ">
         <img src={logo} className="App-logo" alt="logo" />
         <DarkMode />
+        <Price />
+        <Total />
+        <Input />
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button type="button" >
             count is: {count}
           </button>
         </p>
